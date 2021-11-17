7ae3bd741ae7dc7e006fe643fcea9d4870d4c58c
shemar100 <anderson.shemar17@gmail.com>
2021-11-16 23:02:55 -0500

    first commit

---
+++ O(n).py
@@ -0,0 +1,3 @@
+# Time complexity: Time complexity is the amount of time required to execute the algorithm.
+# Space complexity: Space complexity is the amount of memory required to execute the algorithm.
+
