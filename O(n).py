3dd071fb8c7f5a18f748a43f86310dc6a8aae9dc
shemar100 <anderson.shemar17@gmail.com>
2021-11-16 23:15:22 -0500

    dropping constants

--- O(n).py
+++ O(n).py
@@ -1,3 +1,14 @@
 # Time complexity: Time complexity is the amount of time required to execute the algorithm.
 # Space complexity: Space complexity is the amount of memory required to execute the algorithm.
 
+def print_items(n):
+    '''
+    ran n + n times or O(2n) but can be rewritten with constant infront of n dopped as O
+    '''
+    for i in range(n):
+        print(i)
+        
+    for j in range(n):
+        print(j)
+        
+print_items(10)
