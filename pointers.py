a2518aeef509a9262c555728f09a9584b0a69ffd
shemar100 <anderson.shemar17@gmail.com>
2021-11-18 12:11:53 -0500

    pointers

---
+++ pointers.py
@@ -0,0 +1,32 @@
+num1 = 11
+
+num2 = num1
+
+print("Before value is udpated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+num1 = 22
+
+print("\nAfter value is udpated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+dict1 = {
+    'value': 11
+}
+
+dict2 = dict1
+
+dict3 = dict2
+
+print("Before value is udpated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+dict1['value'] = 22
+
+print('/nAfter value is updated:')
+print('dict1 =', dict1)
+print('dict2 =', dict2)
+
