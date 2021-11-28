d8791b59af26a9aaceeeb178e98f637735637637
shemar100 <anderson.shemar17@gmail.com>
2021-11-27 19:38:14 -0500

    stack push method

--- DataStructures/stack.py
+++ DataStructures/stack.py
@@ -8,14 +8,26 @@ class Stack:
         new_node = Node(value)
         self.top = new_node
         self.height = 1
-    
+        
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        
     def print_stack(self):
         temp = self.top
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
             
 
 new_stack = Stack(1)
+new_stack.push(2)
+new_stack.push(4)
 
 new_stack.print_stack()
