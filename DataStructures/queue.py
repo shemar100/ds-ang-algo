5230fd9ceef03528d9c2611fe39a7ce11d560506
shemar100 <anderson.shemar17@gmail.com>
2021-11-27 22:56:41 -0500

    dequeue

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -9,8 +9,8 @@ class Queue:
         new_node = Node(value)
         self.first = new_node
         self.last = new_node
-        self.lenth = 1
-        
+        self.length = 1
+     #   
     def enqueue(self, value):
         new_node = Node(value)
         if self.first is None:
@@ -19,8 +19,24 @@ class Queue:
         else:
             self.last.next = new_node
             self.last = new_node
-        self.lenth += 1
+        self.length += 1
         
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+            
+        self.length -= 1
+        return temp
+            
+            
         
     def print_queue(self):
         temp = self.first
@@ -32,3 +48,4 @@ my_queue = Queue(1)
 my_queue.enqueue(2)
         
 my_queue.print_queue()
+print('removed', my_queue.dequeue().value)
