9a0e693731774b6a0fc28542d71473de8ef02129
shemar100 <anderson.shemar17@gmail.com>
2021-11-29 12:00:40 -0500

    bubble sort

---
+++ Alogoritm/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list) -1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+    return my_list
+
+
+my_list = [10, 134, 0, 84, 4, 1, 100, 4, 9]
+
+print(bubble_sort(my_list))
