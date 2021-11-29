01c4810a38f61c1ca1a88eaba47a1c22b3d0caf1
shemar100 <anderson.shemar17@gmail.com>
2021-11-28 23:15:49 -0500

    add edge graph

---
+++ DataStructures/graphs.py
@@ -0,0 +1,34 @@
+class Graph:
+    # Create graph constructor
+    def __init__(self):
+        self.adj_list = {}
+        
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex]) #getting vertex with edges
+
+    # adds a vertex
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+
+
+my_graph = Graph()
+my_graph.add_vertex("A")
+my_graph.add_vertex("B")
+
+my_graph.add_edge("B", "A")
+
+my_graph.print_graph()
+
+
