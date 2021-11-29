a40436f8a9a13474ecddef6e40d011c9c3ebcf55
shemar100 <anderson.shemar17@gmail.com>
2021-11-29 17:57:46 -0500

    insertion sort

---
+++ Alogoritm/insertion_sort.py
@@ -0,0 +1,11 @@
+def insert_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i-1
+        while temp < my_list[j] and j > -1:
+            my_list[j+1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+            
+print(insert_sort(my_list = [900, 1, 899, 34, 100, 140, 34]))
