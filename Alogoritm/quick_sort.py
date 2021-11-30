a475cecc349cbe887530096b437fa1c5bd9dc53d
shemar100 <anderson.shemar17@gmail.com>
2021-11-29 19:56:47 -0500

    quick sort

--- Alogoritm/quick_sort.py
+++ Alogoritm/quick_sort.py
@@ -13,3 +13,14 @@ def pivot(my_list, pivot_index, end_index):
     
     return swap_index
 
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index-1)
+        quick_sort_helper(my_list, pivot_index +1, right)
+    return my_list
+
+def quick_sort(my_list):
+    return quick_sort_helper(my_list, 0, len(my_list)-1)
+
+print(quick_sort([4,6,1,7,3,2,5]))
